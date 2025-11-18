6c9fc6dd8ad8886a1b6bd8ff2a76751d4e92a8c9
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-11-18 12:42:19 -0600

    Subida de actividad de Roombas, simulación 1 y simulación 2

---
+++ Actividad_2_Roomba/Simulacion_1/random_agents/model.py
@@ -0,0 +1,78 @@
+from mesa import Model
+from mesa.discrete_space import OrthogonalMooreGrid
+
+from .agent import RandomAgent, ObstacleAgent, DirtyAgent, ChargingStation, dirty_agents
+
+class RandomModel(Model):
+    """
+    Creates a new model with random agents.
+    Args:
+        num_agents: Number of agents in the simulation
+        height, width: The size of the grid to model
+    """
+    def __init__(self, num_agents=1, width=8, height=8, seed=42, dirty_cells=10, num_obstacles=10):
+
+        super().__init__(seed=seed)
+        self.num_agents = num_agents
+        self.num_obstacles = num_obstacles
+        self.seed = seed
+        self.dirty_cells = dirty_cells
+        self.width = width
+        self.height = height
+
+        self.grid = OrthogonalMooreGrid([width, height], torus=False)
+
+        # Limpiamos la lista global de dirty_agents al iniciar el modelo para crear los nuevos random
+        dirty_agents.clear()
+
+        offset_y = 25  # Ponemos estacion de carga hasta arriba
+
+        border = [(x, y + offset_y) 
+                for y in range(3)
+                for x in range(3)
+                if y in [0, 2] or x in [0, 2]]  
+
+        border2 = [(x, y + offset_y)
+                for y in range(2)
+                for x in range(2)
+                if y in [0, 1] or x in [0, 1]]  
+
+        for _, cell in enumerate(self.grid):
+            if cell.coordinate == (1,1): ## Usamos ambas listas
+        # Generamos agentes de limpieza pero en la posición 1,1
+                RandomAgent.create_agents(
+                    self,
+                    self.num_agents,
+                    cell=[cell]
+                )
+                break
+        
+        
+        # Creamos la estación de carga
+        for _, cell in enumerate(self.grid):
+            if cell.coordinate in border+border2: ## Usamos ambas listas
+                ChargingStation(self, cell=cell)
+            
+
+        
+        ## Generamos obstaculos apartir de num of obstacles
+        for i in range(self.num_obstacles):
+            ObstacleAgent(self, cell=self.random.choices(self.grid.empties.cells)[0])
+
+
+
+
+        # estaciones_carga = self.random.choices(self.grid.empties.cells, k=self.num_agents)
+        # for cell in estaciones_carga:
+        #     ChargingStation(self, cell=cell)
+
+        for i in range(self.dirty_cells):
+            # Agregamos dirty cells
+            DirtyAgent(self, cell=self.random.choices(self.grid.empties.cells)[0])
+        
+    
+        self.running = True
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.agents.shuffle_do("step")
